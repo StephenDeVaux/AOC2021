5a50514bd561131b3d5ad6ff4531ec8f47902d9a
Stevey <60765528+StephenDeVaux@users.noreply.github.com>
2021-12-07 06:48:19 +1100

    cleaned up

--- src/day6.js
+++ src/day6.js
@@ -1,6 +1,4 @@
-const { Console } = require("console");
 const fs = require("fs");
-const { Agent } = require("http");
 const inputCsv = "./data/day6data.csv";
 // const inputCsv = "./data/day6dataTest.csv";
 
@@ -11,53 +9,23 @@ const readCSV = async () => {
   console.log(inputData);
 
   let fish = inputData[0].split(",").map((fish) => parseInt(fish));
-  console.log(fish);
-
-  // let notCountedFish = [];
-
-  // for (let i = 0; i < fish.length; i++) {}
 
   console.log("age arra : ");
   console.log(createAgeArray(fish));
 
   let ageArray = createAgeArray(fish);
-  let countedFish = 0;
   for (let day = 0; day < 256; day++) {
     let numFish = ageArray.shift();
     if (numFish === undefined) continue;
     console.log(numFish);
     ageArray[8] = numFish;
     ageArray[6] = (ageArray[6] === undefined ? 0 : ageArray[6]) + numFish;
-    countedFish = countedFish + numFish;
   }
   console.log(ageArray);
-  console.log(countedFish);
   console.log('fish in array = ' + countFinalFish(ageArray))
-  // console.log('total fish = ' + (countedFish + countFinalFish(ageArray)))
-  // console.log('total fish = ' + (countedFish + countFinalFish(ageArray)))
 };
 
 module.exports = readCSV;
-
-const dayOfLife = (fishes) => {
-  let babyFish = [];
-  let olderFish = fishes.map((fish) => {
-    if (fish === 0) {
-      babyFish.push(8);
-      return 6;
-    }
-    return fish - 1;
-  });
-
-  return [...olderFish, ...babyFish];
-};
-
-const numBabys = (fishAge, days) => {
-  let babes = Math.ceil((days - fishAge) / 7);
-  return babes > 0 ? babes : 0;
-};
-
-const babiesBabies = (numBabys, days) => {};
 
 const createAgeArray = (fishes) => {
   let ageArray = [];
@@ -69,16 +37,6 @@ const createAgeArray = (fishes) => {
     }
   });
   return ageArray;
-};
-
-const ageOneDay = (ageArray) => {
-  let numFish = ageArray.shift();
-  ageArray[9] = numFish;
-
-  return {
-    ageArray: ageArray,
-    countedFish: numFish,
-  };
 };
 
 const countFinalFish = (array) => {
